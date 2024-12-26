94cd1802f7cdfb304f5bb06b878ebb81945430ee
Domagoj <a.domagoj@hotmail.com>
2024-12-26 19:50:03 +0100

    Setup for the nvidia graphics driver so I can play some games...

--- home.nix
+++ home.nix
@@ -64,6 +64,7 @@
     bash
     zsh
     copyq
+    lshw
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
