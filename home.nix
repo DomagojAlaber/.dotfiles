853736a04e6781bb349dd1f98a50b0aa2c313056
Domagoj <a.domagoj@hotmail.com>
2025-04-07 22:15:55 +0200

    Drawio

--- home.nix
+++ home.nix
@@ -73,7 +73,7 @@
     libgcc
     sqlite
     gcc
-    notion-app-enhanced
+    drawio
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
