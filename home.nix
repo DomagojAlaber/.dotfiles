80f978b0d1320c84273616465e7248698b2cbc96
Domagoj Alaber <a.domagoj@hotmail.com>
2025-04-23 18:48:39 +0200

    Prism launcher for Minecraft. Something just feels right about installing Minecraft like this.

--- home.nix
+++ home.nix
@@ -70,6 +70,7 @@
     libgcc
     tmux
     drawio
+    prismlauncher-unwrapped
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
