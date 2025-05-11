6ac0c1076be5abcad99b10b3669160b32223b910
Domagoj Alaber <a.domagoj@hotmail.com>
2025-05-11 14:19:13 +0200

    Sweet home 3D

--- home.nix
+++ home.nix
@@ -71,6 +71,7 @@
     drawio
     prismlauncher-unwrapped
     jdk
+    sweethome3d.application
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
