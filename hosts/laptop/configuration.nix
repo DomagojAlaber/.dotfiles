af6cc4c75e18ae179e2acd49f0b0d64bdb95409d
Domagoj <a.domagoj@hotmail.com>
2025-11-10 22:39:07 +0100

    New desktopManager options

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -83,9 +83,8 @@
 
   # Enable the GNOME Desktop Environment.
   # Uncomment these two lines if all else fails
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 
   services.blueman.enable = true;
   # Configure keymap in X11
