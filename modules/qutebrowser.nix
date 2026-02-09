adcc39ee4e264ca7d3f04319e259ad0a3f4e6e73
Domagoj <a.domagoj@hotmail.com>
2026-02-09 18:46:33 +0100

    A little change to qutebrowser

--- modules/qutebrowser.nix
+++ modules/qutebrowser.nix
@@ -5,6 +5,10 @@
     enable = true;
     settings = {
       tabs.position = "left";
+      tabs.favicons.show = "always";
+      tabs.title.format = "";
+      tabs.title.format_pinned = "";
+      tabs.width = 44;
       colors.webpage.darkmode.enabled = true;
       colors.webpage.preferred_color_scheme = "dark";
     };
