6b7a60f57b90aaca7122e42223fa47bde5e9df2e
Domagoj <a.domagoj@hotmail.com>
2026-04-15 22:22:01 +0200

    Codex package update

--- modules/codex.nix
+++ modules/codex.nix
@@ -27,7 +27,7 @@ in
   programs.codex = {
     enable = true;
 
-    custom-instructions = ''
+    context = ''
       You are able to use the Svelte MCP server, where you have access to comprehensive Svelte 5 and SvelteKit documentation. Here's how to use the available tools effectively:
 
       ## Available MCP Tools:
